ac3dbfb61d4382f995dd21ee2c8d523cf26408a3
Алекс <75216201+defini7@users.noreply.github.com>
2022-09-12 21:57:44 +0300

    Update XBOX_Controller.h

--- XBOX_Controller.h
+++ XBOX_Controller.h
@@ -4,7 +4,7 @@
 /***
 *	BSD 3-Clause License
 
-	Copyright (c) 2021, Alex
+	Copyright (c) 2021, 2022 Alex
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
