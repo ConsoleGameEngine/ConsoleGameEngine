4c93401506a167c1e66f57e04d81939acbf1d904
Alex <75216201+defini7@users.noreply.github.com>
2023-04-30 17:09:48 +0300

    Update Tetris.cpp

--- demos/Tetris.cpp
+++ demos/Tetris.cpp
@@ -41,58 +41,64 @@ private:
 	list<int> listLines;
 
 protected:
-	int Rotate(int px, int py, int r)
+	int Rotate(int x, int y, int r)
 	{
 		switch (r % 4)
 		{
-		case 0: return py * 4 + px;
-		case 1: return 12 + py - px * 4;
-		case 2: return 15 - py * 4 - px;
-		case 3: return 3 - py + px * 4;
+		case 0: return y * 4 + x;
+		case 1: return 12 + y - x * 4;
+		case 2: return 15 - y * 4 - x;
+		case 3: return 3 - y + x * 4;
 		}
 
 		return 0;
 	}
 
-	bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+	bool DoesPieceFit(int t, int r, int x, int y)
 	{
 		for (int i = 0; i < 4; i++)
+		{
 			for (int j = 0; j < 4; j++)
 			{
-				int p = Rotate(i, j, nRotation);
-				int a = (nPosY + j) * nAreaWidth + (nPosX + i);
+				int p = Rotate(i, j, r);
+				int a = (y + j) * nAreaWidth + (x + i);
 
-				if (nPosX + i >= 0 && nPosX + i < nAreaWidth)
-					if (nPosY + j >= 0 && nPosY + j < nAreaHeight)
-						if (tetromino[nTetromino][p] != L'.' && pArea[a] != 0)
-							return false;
+				if (x + i >= 0 && x + i < nAreaWidth &&
+					y + j >= 0 && y + j < nAreaHeight)
+				{
+					if (tetromino[t][p] != L'.' && pArea[a] != 0)
+						return false;
+				}
 			}
+		}
 
 		return true;
 	}
 
-	bool OnUserCreate() override
+	virtual bool OnUserCreate() override
 	{
 		srand(time(0));
 
-		tetromino[0].append("..X...X...X...X.");
-		tetromino[1].append("..X..XX...X.....");
-		tetromino[2].append(".....XX..XX.....");
-		tetromino[3].append("..X..XX..X......");
-		tetromino[4].append(".X...XX...X.....");
-		tetromino[5].append(".X...X...XX.....");
-		tetromino[6].append("..X...X..XX.....");
+		tetromino[0] = "..X...X...X...X.";
+		tetromino[1] = "..X..XX...X.....";
+		tetromino[2] = ".....XX..XX.....";
+		tetromino[3] = "..X..XX..X......";
+		tetromino[4] = ".X...XX...X.....";
+		tetromino[5] = ".X...X...XX.....";
+		tetromino[6] = "..X...X..XX.....";
 
 		pArea = new unsigned char[nAreaWidth * nAreaHeight];
 
 		for (int x = 0; x < nAreaWidth; x++)
+		{
 			for (int y = 0; y < nAreaHeight; y++)
 				pArea[y * nAreaWidth + x] = (x == 0 || x == nAreaWidth - 1 || y == nAreaHeight - 1) ? 9 : 0;
+		}
 
 		return true;
 	}
 
-	bool OnUserUpdate(float fDeltaTime) override
+	virtual bool OnUserUpdate(float fDeltaTime) override
 	{
 		Clear(PIXEL_SOLID, FG_BLACK);
 
@@ -107,45 +113,52 @@ protected:
 		nSpeedCount++;
 		bForceDown = (nSpeedCount == nSpeed);
 
-		nCurrentX += (GetKey(VK_RIGHT).bHeld && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY)) ? 1 : 0;
-		nCurrentX -= (GetKey(VK_LEFT).bHeld && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY)) ? 1 : 0;
-		nCurrentY += (GetKey(VK_DOWN).bHeld && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1)) ? 1 : 0;
+		nCurrentX += (int)(GetKey(VK_RIGHT).bHeld && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY));
+		nCurrentX -= (int)(GetKey(VK_LEFT).bHeld && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY));
+		nCurrentY += (int)(GetKey(VK_DOWN).bHeld && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1));
 
 		if (GetMouse(0).bPressed)
-			nCurrentRotation += DoesPieceFit(nCurrentPiece, nCurrentRotation + 1, nCurrentX, nCurrentY) ? 1 : 0;
+			nCurrentRotation += (int)DoesPieceFit(nCurrentPiece, nCurrentRotation + 1, nCurrentX, nCurrentY);
 
 		if (bForceDown)
 		{
 			nSpeedCount = 0;
 			nPieceCount++;
 
-			if (nPieceCount % 50 == 0)
-				if (nSpeed >= 10) nSpeed--;
+			if (nPieceCount % 50 == 0 && nSpeed >= 10)
+				nSpeed--;
 
 			if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1))
 				nCurrentY++;
 			else
 			{
 				for (int px = 0; px < 4; px++)
+				{
 					for (int py = 0; py < 4; py++)
+					{
 						if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] != L'.')
 							pArea[(nCurrentY + py) * nAreaWidth + (nCurrentX + px)] = nCurrentPiece + 1;
+					}
+				}
 
 				for (int py = 0; py < 4; py++)
+				{
 					if (nCurrentY + py < nAreaHeight - 1)
 					{
 						bool bLine = true;
 
 						for (int px = 1; px < nAreaWidth - 1; px++)
-							bLine &= (pArea[(nCurrentY + py) * nAreaWidth + px]) != 0;
+							bLine &= ((pArea[(nCurrentY + py) * nAreaWidth + px]) != 0);
 
 						if (bLine)
 						{
 							for (int px = 1; px < nAreaWidth - 1; px++)
 								pArea[(nCurrentY + py) * nAreaWidth + px] = 8;
+
 							listLines.push_back(nCurrentY + py);
 						}
 					}
+				}
 
 				nScore += 25;
 				if (!listLines.empty())	nScore += (1 << listLines.size()) * 100;
@@ -160,13 +173,20 @@ protected:
 		}
 
 		for (int i = 0; i < nAreaWidth; i++)
+		{
 			for (int j = 0; j < nAreaHeight; j++)
 				Draw(i + 2, j + 2, PIXEL_SOLID, pArea[j * nAreaWidth + i]);
+		}
+				
 
 		for (int i = 0; i < 4; i++)
+		{
 			for (int j = 0; j < 4; j++)
+			{
 				if (tetromino[nCurrentPiece][Rotate(i, j, nCurrentRotation)] != '.')
 					Draw(nCurrentX + i + 2, nCurrentY + j + 2, PIXEL_SOLID, nCurrentPiece + 1);
+			}
+		}
 
 		DrawString(15, 1, L"Score:" + to_wstring(nScore));
 
