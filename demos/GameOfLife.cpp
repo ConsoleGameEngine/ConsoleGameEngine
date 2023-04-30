46635b0f1ab649e0031c7c306d683002f106549c
Alex <75216201+defini7@users.noreply.github.com>
2023-04-30 17:21:22 +0300

    Update GameOfLife.cpp

--- demos/GameOfLife.cpp
+++ demos/GameOfLife.cpp
@@ -1,8 +1,8 @@
 #include "ConsoleGameEngine.h"
 
-using namespace def;
+using namespace std;
 
-class CellularAutomata : public def::ConsoleGameEngine
+class CellularAutomata : public ConsoleGameEngine
 {
 public:
 	CellularAutomata()
@@ -11,8 +11,8 @@ public:
 	}
 
 private:
-	bool* output;
-	bool* state;
+	bool* output = nullptr;
+	bool* state = nullptr;
 
 private:
 	int GetNeighboursCount(int x, int y)
@@ -20,12 +20,12 @@ private:
 		int nCount = 0;
 
 		for (int i = -1; i <= 1; i++)
+		{
 			for (int j = -1; j <= 1; j++)
-				nCount += output[(y + j) * GetScreenWidth() + (x + i)];
+				nCount += output[(y + j) * ScreenWidth() + (x + i)];
+		}
 
-		// Because we don't count center cell
-		if (output[y * GetScreenWidth() + x] == 1)
-			nCount--;
+		if (output[y * ScreenWidth() + x] == 1) nCount--;
 
 		return nCount;
 	}
@@ -33,14 +33,14 @@ private:
 protected:
 	bool OnUserCreate() override
 	{
-		output = new bool[GetScreenWidth() * GetScreenHeight()];
-		state = new bool[GetScreenWidth() * GetScreenHeight()];
+		output = new bool[ScreenWidth() * ScreenHeight()];
+		state = new bool[ScreenWidth() * ScreenHeight()];
 
-		memset(output, 0, GetScreenWidth() * GetScreenHeight() * sizeof(bool));
-		memset(state, 0, GetScreenWidth() * GetScreenHeight() * sizeof(bool));
+		memset(output, 0, ScreenWidth() * ScreenHeight() * sizeof(bool));
+		memset(state, 0, ScreenWidth() * ScreenHeight() * sizeof(bool));
 
-		for (int i = 0; i < GetScreenWidth() * GetScreenHeight(); i++)
-			state[i] = rand() % 2;
+		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
+			state[i] = (bool)(rand() % 2);
 
 		return true;
 	}
@@ -52,26 +52,25 @@ protected:
 
 		Clear(PIXEL_SOLID, FG_BLACK);
 
-		DrawRectangle(0, 0, GetScreenWidth() - 1, GetScreenHeight() - 1, PIXEL_SOLID, FG_RED);
+		DrawRectangle(0, 0, ScreenWidth() - 1, ScreenHeight() - 1, PIXEL_SOLID, FG_RED);
 
-		// Make a delay to see work of algorithm
-		using namespace std::chrono_literals;
-		std::this_thread::sleep_for(75ms);
+		//using namespace chrono_literals;
+		//this_thread::sleep_for(75ms);
 
-		for (int i = 0; i < GetScreenWidth() * GetScreenHeight(); i++)
+		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
 			output[i] = state[i];
 
-		for (int x = 1; x < GetScreenWidth() - 1; x++)
-			for (int y = 1; y < GetScreenHeight() - 1; y++)
+		for (int x = 1; x < ScreenWidth() - 1; x++)
+			for (int y = 1; y < ScreenHeight() - 1; y++)
 			{
 				int nCount = GetNeighboursCount(x, y);
 
-				if (output[y * GetScreenWidth() + x] == 1)
-					state[y * GetScreenWidth() + x] = (nCount == 2 || nCount == 3);
+				if (output[y * ScreenWidth() + x] == 1)
+					state[y * ScreenWidth() + x] = (nCount == 2 || nCount == 3);
 				else
-					state[y * GetScreenWidth() + x] = (nCount == 3);
+					state[y * ScreenWidth() + x] = (nCount == 3);
 
-				if (output[y * GetScreenWidth() + x] == 1)
+				if (output[y * ScreenWidth() + x] == 1)
 					Draw(x, y, PIXEL_SOLID, FG_WHITE);
 
 			}
@@ -86,12 +85,8 @@ int main()
 
 	CellularAutomata game;
 
-	def::rcode err = game.ConstructConsole(160, 100, 8, 8);
-
-	if (err.ok)
+	if (game.ConstructConsole(160, 100, 8, 8) == RCODE_OK)
 		game.Run();
-	else
-		std::cerr << err.info << "\n";
 
 	return 0;
 }
