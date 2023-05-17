7819c0b135567fc7708d21f1d190b6fc07a7b0da
Alex <75216201+defini7@users.noreply.github.com>
2023-05-17 23:25:26 +0300

    Update GameOfLife.cpp

--- demos/GameOfLife.cpp
+++ demos/GameOfLife.cpp
@@ -1,18 +1,23 @@
-#include "ConsoleGameEngine.h"
+#define DGE_APPLICATION
+#include "defGameEngine.h"
+
+#include <thread>
 
 using namespace std;
 
-class CellularAutomata : public ConsoleGameEngine
+class CellularAutomata : public def::GameEngine
 {
 public:
 	CellularAutomata()
 	{
-		sAppName = L"Cellular Automata";
+		SetTitle("Cellular Automata");
 	}
 
 private:
 	bool* output = nullptr;
 	bool* state = nullptr;
+
+	float fTicks = 0.0f;
 
 private:
 	int GetNeighboursCount(int x, int y)
@@ -39,41 +44,60 @@ protected:
 		memset(output, 0, ScreenWidth() * ScreenHeight() * sizeof(bool));
 		memset(state, 0, ScreenWidth() * ScreenHeight() * sizeof(bool));
 
-		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
-			state[i] = (bool)(rand() % 2);
+		/*for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
+			state[i] = (bool)(rand() % 2);*/
+
+		auto set = [&](int x, int y, std::string v)
+		{
+			for (int i = 0; i < v.size(); i++)
+				state[y * ScreenWidth() + (x + i)] = (v[i] == '#') ? true : false;
+		};
+
+		set(60, 45, "........................#............");
+		set(60, 46, "......................#.#............");
+		set(60, 47, "............##......##............##.");
+		set(60, 48, "...........#...#....##............##.");
+		set(60, 49, "##........#.....#...##...............");
+		set(60, 50, "##........#...#.##....#.#............");
+		set(60, 51, "..........#.....#.......#............");
+		set(60, 52, "...........#...#.....................");
+		set(60, 53, "............##.......................");
 
 		return true;
 	}
 
-	bool OnUserUpdate(float fElapsedTime) override
+	bool OnUserUpdate(float fDeltaTime) override
 	{
-		if (GetKey(VK_SPACE).bHeld)
+		if (GetKey(def::Key::SPACE).bHeld)
 			return true;
 
-		Clear(PIXEL_SOLID, FG_BLACK);
+		fTicks += fDeltaTime;
+		if (fTicks > 0.1f)
+		{
+			fTicks = 0.0f;
 
-		DrawRectangle(0, 0, ScreenWidth() - 1, ScreenHeight() - 1, PIXEL_SOLID, FG_RED);
+			Clear(def::BLACK);
 
-		//using namespace chrono_literals;
-		//this_thread::sleep_for(75ms);
+			DrawRectangle(0, 0, ScreenWidth() - 1, ScreenHeight() - 1, def::RED);
 
-		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
-			output[i] = state[i];
+			for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
+				output[i] = state[i];
 
-		for (int x = 1; x < ScreenWidth() - 1; x++)
-			for (int y = 1; y < ScreenHeight() - 1; y++)
-			{
-				int nCount = GetNeighboursCount(x, y);
+			for (int x = 1; x < ScreenWidth() - 1; x++)
+				for (int y = 1; y < ScreenHeight() - 1; y++)
+				{
+					int nCount = GetNeighboursCount(x, y);
 
-				if (output[y * ScreenWidth() + x] == 1)
-					state[y * ScreenWidth() + x] = (nCount == 2 || nCount == 3);
-				else
-					state[y * ScreenWidth() + x] = (nCount == 3);
+					if (output[y * ScreenWidth() + x] == 1)
+						state[y * ScreenWidth() + x] = (nCount == 2 || nCount == 3);
+					else
+						state[y * ScreenWidth() + x] = (nCount == 3);
 
-				if (output[y * ScreenWidth() + x] == 1)
-					Draw(x, y, PIXEL_SOLID, FG_WHITE);
+					if (output[y * ScreenWidth() + x] == 1)
+						Draw(x, y, def::WHITE);
 
-			}
+				}
+		}
 
 		return true;
 	}
@@ -85,7 +109,7 @@ int main()
 
 	CellularAutomata game;
 
-	if (game.ConstructConsole(160, 100, 8, 8) == RCODE_OK)
+	if (game.Construct(160, 100, 8, 8))
 		game.Run();
 
 	return 0;
